26355b874b7c7b07d51066a372b145b24f477b59
Chieh Teng Wang <chieh.wang@bonial.com>
2020-10-12 17:30:07 +0200

    12%

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ myProj.parse(function (err) {
 
     // Specify number of tests for test plans
     log('Total number of tests: ', totalNumberOfTests);
-    const percentageForFirstPlan = SHARDS == 1 ? 100 : ( SHARDS == 2 ? 30 : 10 );
+    const percentageForFirstPlan = SHARDS == 1 ? 100 : ( SHARDS == 2 ? 30 : 12 );
     const fixedNumberOfTestsForFirstPlan = Math.round(totalNumberOfTests / 100 * percentageForFirstPlan);
     const numberOfTestsForOtherPlans = Math.round((totalNumberOfTests - fixedNumberOfTestsForFirstPlan) / (SHARDS - 1));
     var allTestClasses = [];
